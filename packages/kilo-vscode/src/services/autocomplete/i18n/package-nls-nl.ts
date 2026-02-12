23580e67b72cdbbae6bdebc43689efeac03b95ea
Mark IJbema <mark@kilocode.ai>
2026-02-12 09:54:12 +0100

    add autocomplete files

---
+++ packages/kilo-vscode/src/services/autocomplete/i18n/package-nls-nl.ts
@@ -0,0 +1,14 @@
+// kilocode_change - new file
+// nl package.nls translations for autocomplete
+
+export const dict = {
+	"autocomplete.input.title": "Druk op 'Enter' om te bevestigen of 'Escape' om te annuleren",
+	"autocomplete.input.placeholder": "Beschrijf wat je wilt doen...",
+	"autocomplete.commands.generateSuggestions": "Kilo Code: Bewerkingssuggesties Genereren",
+	"autocomplete.commands.displaySuggestions": "Bewerkingssuggesties Weergeven",
+	"autocomplete.commands.cancelSuggestions": "Bewerkingssuggesties Annuleren",
+	"autocomplete.commands.applyCurrentSuggestion": "Huidige Bewerkingssuggestie Toepassen",
+	"autocomplete.commands.applyAllSuggestions": "Alle Bewerkingssuggesties Toepassen",
+	"autocomplete.commands.goToNextSuggestion": "Ga Naar Volgende Suggestie",
+	"autocomplete.commands.goToPreviousSuggestion": "Ga Naar Vorige Suggestie",
+}
